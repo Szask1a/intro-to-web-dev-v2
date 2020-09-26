4fbd231cf8df1264e6286497839a138bff9493f7
Abi Karunaratne <Szask1a@users.noreply.github.com>
2020-09-26 18:21:30 +0530

    Arrays and Functions

--- Javascript Exercises/jsFundementals.js
+++ Javascript Exercises/jsFundementals.js
@@ -2,4 +2,61 @@
 
 const dickSize = 100;
 const chad = 10 * dickSize;
+const firstname = 'Abi';
+const lastname = 'Karunaratne';
+
+//Concatonating Strings
+const sentence = 'Hello,' + firstname + " " + lastname + " Welcome! ";
+
+//To use template strings use backticks
+const sentenceTemplate = `Hello, ${firstname} ${lastname} Welcome!`;
 console.log(chad);
+
+//Booleans
+const randomBool = true;
+const anotherBool = false;
+
+console.log(randomBool);
+console.log(anotherBool);
+
+// Control Flow (if)
+
+const temp = 86;
+
+if (temp >= 80) {
+    console.log(temp);
+} else {
+    console.log(temp);
+}
+
+// let = local variable
+// var = global variable
+
+//Functions
+
+function greet(name, age) {
+    return `greetings ${name} your age is ${age}`
+  }
+  console.log(greet('abi', '21'));
+
+  //objects
+  //objects can have functions and other objects nested inside them
+
+  const student = {
+      id: 100,
+      studentName: 'abi',
+  };
+
+  console.log(student);
+  console.log(student.id);
+
+  //Arrays
+
+  const numbers = [
+      '1',
+      '2',
+      '3',
+  ]
+
+  console.log(numbers);
+  console.log(numbers[1]);
